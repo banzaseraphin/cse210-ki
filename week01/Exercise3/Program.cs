99b19cfa08972893517096a0fe59726cd64cd6b9
banzaseraphin <seraphimkiongo@.com>
2025-10-31 01:15:52 +0200

    changes

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Guess a number between 1 and 101:");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the number!");
+            }
+        }
     }
 }
